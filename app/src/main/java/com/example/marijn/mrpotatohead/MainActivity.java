c3b23dafc8979d2e7a0ed84f314bc6d4d21e0699
Marijn Meijering <rijno95@hotmail.com>
2018-11-01 23:36:54 +0100

    Second commit

--- app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.marijn.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Check if checkbox is checked by user
+    public void checkClicked(View v) {
+
+        CheckBox checkbox = (CheckBox) v;
+
+        String potatoPlaceholder = checkbox.getText().toString();
+
+        // Get resource ID by using the resources string name.
+        // Source: https://android.okhelp.cz/get-resource-id-by-resources-string-name-android-example/
+        int resourceID = getResources().getIdentifier(potatoPlaceholder, "id", getPackageName());
+        ImageView potatoImage = findViewById(resourceID);
+
+        // Hide or show image
+        if (potatoImage.getVisibility() == View.INVISIBLE){
+            potatoImage.setVisibility(View.VISIBLE);
+        }
+        else if (potatoImage.getVisibility() == View.VISIBLE) {
+            potatoImage.setVisibility(View.INVISIBLE);
+        }
     }
 }
