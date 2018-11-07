8b2926da59bea40cb0fdac5756443bf77a3c696d
10810765 <10810765@users.noreply.github.com>
2018-11-07 23:09:50 +0100

    Added SaveState functionality

--- app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
@@ -9,10 +9,33 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Create a list with all Mr Potatohead parts
+    String[] dressPotato = {"arms", "ears", "eyebrows", "eyes", "glasses", "hat", "mouth", "mustache", "nose", "shoes"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            return;
+        } else {
+            for (String bodyP : dressPotato) {
+                int resourceID = getResources().getIdentifier(bodyP, "id", getPackageName());
+                ImageView image = findViewById(resourceID);
+                image.setVisibility(savedInstanceState.getInt(bodyP));
+            }
+        }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState); // always call super
+
+        // Save the state with outState
+        for (String bodyP: dressPotato) {
+            int resourceID = getResources().getIdentifier(bodyP, "id", getPackageName());
+            ImageView image = findViewById(resourceID);
+            outState.putInt(bodyP, image.getVisibility());
+        }
     }
 
     // Check if checkbox is checked by user
