1bc5a358967dacfc416e9d52a23b4a6fa12d8243
Marijn Meijering <rijno95@hotmail.com>
2018-12-12 17:24:49 +0100

    Final commit, some changes to the code

--- app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/marijn/mrpotatohead/MainActivity.java
@@ -6,49 +6,55 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+/**
+ * Marijn Meijering <m.h.j.meijering@uva.nl>
+ * 10810765 Universiteit van Amsterdam
+ * Minor Programmeren 17/12/2018
+ */
 public class MainActivity extends AppCompatActivity {
 
     // Create a list with all Mr Potatohead parts
-    String[] dressPotato = {"arms", "ears", "eyebrows", "eyes", "glasses", "hat", "mouth", "mustache", "nose", "shoes"};
-    String[] checkBoxes = {"checkArms", "checkEars", "checkEyebrows", "checkEyes", "checkGlasses", "checkHat", "checkMouth", "checkMustache", "checkNose", "checkShoes"};
-
+    String[] mrPotatoParts = {"arms", "ears", "eyebrows", "eyes", "glasses", "hat", "mouth", "mustache", "nose", "shoes"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // If there is a saved instance, reload these states
         if (savedInstanceState == null) {
             return;
         } else {
-            for (String bodyP : dressPotato) {
-                int resourceID = getResources().getIdentifier(bodyP, "id", getPackageName());
+            for (String potatoP : mrPotatoParts) {
+                int resourceID = getResources().getIdentifier(potatoP, "id", getPackageName());
                 ImageView image = findViewById(resourceID);
-                image.setVisibility(savedInstanceState.getInt(bodyP));
+                image.setVisibility(savedInstanceState.getInt(potatoP));
             }
         }
     }
 
     public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState); // always call super
+        super.onSaveInstanceState(outState);
 
         // Save the state with outState
-        for (String bodyP: dressPotato) {
-            int resourceID = getResources().getIdentifier(bodyP, "id", getPackageName());
+        for (String potatoP: mrPotatoParts) {
+            int resourceID = getResources().getIdentifier(potatoP, "id", getPackageName());
             ImageView image = findViewById(resourceID);
-            outState.putInt(bodyP, image.getVisibility());
+            outState.putInt(potatoP, image.getVisibility());
         }
     }
 
-    // On reset button clicked, reset checkboxes and image views
+    // On reset button clicked, reset checkboxes and set image views invisible
     public void resetClicked(View v) {
-        for (String bodyP : dressPotato) {
-            int resourceID = getResources().getIdentifier(bodyP, "id", getPackageName());
-            ImageView image = findViewById(resourceID);
+        for (String potatoP : mrPotatoParts) {
+            // Reset image visibility
+            int imageID = getResources().getIdentifier(potatoP, "id", getPackageName());
+            ImageView image = findViewById(imageID);
             image.setVisibility(View.INVISIBLE);
-        }
-        for (String checkB : checkBoxes) {
-            int resourceID = getResources().getIdentifier(checkB, "id", getPackageName());
-            CheckBox checkBox = findViewById(resourceID);
+
+            // Reset checkboxes
+            int checkboxID = getResources().getIdentifier(potatoP + "Check", "id", getPackageName());
+            CheckBox checkBox = findViewById(checkboxID);
             checkBox.setChecked(false);
         }
     }
@@ -56,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
     // Check if checkbox is checked by user
     public void checkClicked(View v) {
 
+        // Create a temporary variable to hold the CheckBox
         CheckBox checkbox = (CheckBox) v;
 
         // Get resource string name
@@ -66,11 +73,11 @@ public class MainActivity extends AppCompatActivity {
         int resourceID = getResources().getIdentifier(potatoVar, "id", getPackageName());
         ImageView potatoImage = findViewById(resourceID);
 
-        // Hide or show image
+        // Hide or show image based on previous state
         if (potatoImage.getVisibility() == View.INVISIBLE){
             potatoImage.setVisibility(View.VISIBLE);
         }
-        else if (potatoImage.getVisibility() == View.VISIBLE) {
+        else {
             potatoImage.setVisibility(View.INVISIBLE);
         }
     }
